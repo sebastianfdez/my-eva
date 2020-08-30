8b5b1c11ea8307b67d300f4db3f5096f5f099265
Sebastian Fernandez <sfernandez1@uc.cl>
2020-08-30 17:04:24 +0200

    create angular app

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HomePageModule } from './home-page/home-page.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomePageModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home-page/home-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+import { HomePageComponent } from './home-page.component';
+
+@NgModule({
+  imports: [],
+  exports: [
+    HomePageComponent,
+  ],
+  declarations: [
+    HomePageComponent,
+  ],
+  providers: [],
+})
+export class HomePageModule { }
